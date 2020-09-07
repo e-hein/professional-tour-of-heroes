6ed37d63dfe1d47bf4faafbc7fa25fb5219bb81d
Emanuel Hein <github@e-hein.de>
2020-09-07 17:08:17 +0200

    (hero-lib) use @company/core

--- libs/hero/src/lib/hero.module.ts
+++ libs/hero/src/lib/hero.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CompanyCoreModule } from '@company/core';
 import { CompanyHeroComponent } from './hero.component';
-
-
 
 @NgModule({
   declarations: [CompanyHeroComponent],
   imports: [
+    CompanyCoreModule,
   ],
   exports: [CompanyHeroComponent]
 })
