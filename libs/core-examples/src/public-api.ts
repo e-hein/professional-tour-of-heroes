c9a0f9936819c997e5411fccc10a37b69e380b58
Emanuel Hein <github@e-hein.de>
2020-09-09 20:43:03 +0200

    (core-examples) remove generated service
    there's no use case for a public service of examples

--- libs/core-examples/src/public-api.ts
+++ libs/core-examples/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of core-examples
  */
 
-export * from './lib/core-examples.service';
 export * from './lib/core-examples.component';
 export * from './lib/core-examples.module';
