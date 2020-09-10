9f7423c1c38fbfe8622a2694520c5c28dbec5c93
Emanuel Hein <github@e-hein.de>
2020-09-10 09:13:55 +0200

    (demo-app) use shared karma conf #1

---
+++ apps/demo/project-info.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+/**
+ * @type { import('../../config/project-info').ProjectInfo }
+ */
+const projectInfo = {
+  name: 'demo',
+  root: __dirname,
+};
+
+module.exports = projectInfo;
