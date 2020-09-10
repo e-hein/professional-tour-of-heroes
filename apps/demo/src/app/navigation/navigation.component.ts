fdea861de3c4516d3a2c65862dec339c6a6c10e5
Emanuel Hein <github@e-hein.de>
2020-09-10 11:31:32 +0200

    ng generate component navigation

---
+++ apps/demo/src/app/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'demo-app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.sass']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
