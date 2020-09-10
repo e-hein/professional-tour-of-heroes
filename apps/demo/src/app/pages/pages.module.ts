57980aa69441370f1c7dbe5c27fd104bd79600a7
Emanuel Hein <github@e-hein.de>
2020-09-10 18:09:30 +0200

    (demo-app) ng generate component welcome-page

--- apps/demo/src/app/pages/pages.module.ts
+++ apps/demo/src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WelcomePageComponent],
   imports: [
     CommonModule
   ]
