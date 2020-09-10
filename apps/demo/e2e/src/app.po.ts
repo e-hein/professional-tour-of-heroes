ec180bc2baf0af56856784228509a3bb04b6d6ff
Emanuel Hein <github@e-hein.de>
2020-09-10 10:09:31 +0200

    (demo-app) change prefix to 'demo-app'

--- apps/demo/e2e/src/app.po.ts
+++ apps/demo/e2e/src/app.po.ts
@@ -2,6 +2,6 @@ import { by, element } from 'protractor';
 
 export class AppPage {
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('demo-app-root .content span')).getText() as Promise<string>;
   }
 }
