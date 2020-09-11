bbb6f10f5a177be8de62d9f17b79e50dfeb219fa
Emanuel Hein <github@e-hein.de>
2020-09-11 15:36:28 +0200

    (hero-examples) remove generated service
    there's no use case for a public service of examples

--- libs/hero-examples/src/public-api.ts
+++ libs/hero-examples/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of hero-examples
  */
 
-export * from './lib/hero-examples.service';
 export * from './lib/hero-examples.component';
 export * from './lib/hero-examples.module';
