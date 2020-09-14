df313ef12e73d9cb3f15ff9a214fad6089464dc9
Emanuel Hein <github@e-hein.de>
2020-09-14 08:56:39 +0200

    removed title

--- libs/hero/testing/hero-editor.component-harness.ts
+++ libs/hero/testing/hero-editor.component-harness.ts
@@ -1,17 +1,8 @@
 import { BaseComponentHarness } from '@company/core/testing';
-import { TestElement } from '@angular/cdk/testing';
 import { MatInputHarness } from '@angular/material/input/testing';
 
 export class HeroEditorComponentHarness extends BaseComponentHarness {
   static hostSelector = 'company-hero-editor';
-
-  getTitle(): Promise<TestElement> {
-    return this.locatorFor('h2')();
-  }
-
-  async getTitleText(): Promise<string> {
-    return (await this.getTitle()).text();
-  }
 
   async getHeroId(): Promise<number | null> {
     const heroIdElement = await this.locatorFor('.hero-id')();
